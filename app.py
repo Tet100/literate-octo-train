24718ed0d2d117f4f88d854ca9e31ca103b53cd7
Nikita <Tet1p0@users.noreply.huggingface.co>
2024-11-06 17:37:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("lol")
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="VongolaChouko/Starcannon-Unleashed-12B-v1.0")# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("VongolaChouko/Starcannon-Unleashed-12B-v1.0")
+model = AutoModelForCausalLM.from_pretrained("VongolaChouko/Starcannon-Unleashed-12B-v1.0")
