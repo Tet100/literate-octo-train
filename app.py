6e8af27be66c77862d1611a1f976339a6cd7d67c
Nikita <Tet1p0@users.noreply.huggingface.co>
2024-11-07 14:06:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from llama_cpp import Llama
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
@@ -23,3 +23,5 @@ def test():
     )
     print("GOIDA")
     print(chat["choices"][0].messages)
+if __name__ == "app.py":
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
